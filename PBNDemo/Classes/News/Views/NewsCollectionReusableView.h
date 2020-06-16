f8f1a488356c2859587bb19cc24071a6abba932d
Willian_chang <changwei@dailyinnovation.biz>
2020-06-16 18:58:28 +0800

    完善news页面

---
+++ PBNDemo/Classes/News/Views/NewsCollectionReusableView.h
@@ -0,0 +1,17 @@
+//
+//  NewsCollectionReusableView.h
+//  PBNDemo
+//
+//  Created by 常威 on 2020/6/16.
+//  Copyright © 2020 常威. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NewsCollectionReusableView : UICollectionReusableView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PBNDemo/Classes/News/Views/NewsCollectionReusableView.m
@@ -0,0 +1,13 @@
+//
+//  NewsCollectionReusableView.m
+//  PBNDemo
+//
+//  Created by 常威 on 2020/6/16.
+//  Copyright © 2020 常威. All rights reserved.
+//
+
+#import "NewsCollectionReusableView.h"
+
+@implementation NewsCollectionReusableView
+
+@end
