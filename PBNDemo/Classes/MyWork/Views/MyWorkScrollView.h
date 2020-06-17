34119cc82f286d62326b0ca92e8476cc1dcad4f5
Willian_chang <changwei@dailyinnovation.biz>
2020-06-17 22:37:53 +0800

    搭建mywork页框架

---
+++ PBNDemo/Classes/MyWork/Views/MyWorkScrollView.h
@@ -0,0 +1,17 @@
+//
+//  MyWorkScrollView.h
+//  PBNDemo
+//
+//  Created by 常威 on 2020/6/17.
+//  Copyright © 2020 常威. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyWorkScrollView : UIScrollView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PBNDemo/Classes/MyWork/Views/MyWorkScrollView.m
@@ -0,0 +1,21 @@
+//
+//  MyWorkScrollView.m
+//  PBNDemo
+//
+//  Created by 常威 on 2020/6/17.
+//  Copyright © 2020 常威. All rights reserved.
+//
+
+#import "MyWorkScrollView.h"
+
+@implementation MyWorkScrollView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
